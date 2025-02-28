7e61f9d46bee4344965ba62107f7f8dcb495f48e
DimaGashko <dimagashko@gmail.com>
2025-02-28 23:56:37 +0200

    post-css

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,4 +1,4 @@
-import '/src/styles/base.css';
+import '@/pages/base-page/base-page.ts';
 import './styles/app.css';
 
 console.log('Hello, world!');
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import autoprefixer from 'autoprefixer'
+import customMedia from 'postcss-custom-media'
+import postcssGlobalData from '@csstools/postcss-global-data'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+   resolve: {
+      alias: {
+         '@': '/src',
+      },
+   },
+   css: {
+      postcss: {
+         plugins: [
+            postcssGlobalData({
+               files: [
+                  './src/styles/media.css'
+               ]
+            }),
+            customMedia(),
+            autoprefixer,
+         ],
+      }
+   },
+});
